658376da371066072fa2866d0cc1d94031fffeea
Brewknight <jmaliaras@gmail.com>
2018-05-16 21:32:04 +0300

    a2 done + htmls to make report

--- a2.py
+++ a2.py
@@ -8,10 +8,10 @@ import itertools as it
 import time
 from harversine import harvesine
 from lcss import lcss
+from itertools import izip
 
 trainSet = pd.read_csv('./sets/train_set.csv', converters={"Trajectory": literal_eval})
 testSet = pd.read_csv('./sets/test_set_a2.csv', converters={"Trajectory": literal_eval})
-#trainSet = trainSet[:100]
 queries = list()
 trains = list()
 
@@ -27,13 +27,19 @@ for traj in trainSet["Trajectory"]:
         yi.append( (el[1], el[2]) )
     trains.append(yi)
 
+fds = list()
 
-start = time.time
+for i in xrange(1, len(queries) + 1):
+    name = "./a2Plots/Test Trip " + str(i) + "/Info.txt"
+    f = open(name, "w+")
+    fds.append(f)
 
 toSort = list()
 TestTrips = list()
 # i iindex in train set
-for query in queries:
+for query, file in izip(queries, fds):
+    start = time.time()
+
     i = 0
     toSort = []
     for traj in trains:
@@ -45,21 +51,57 @@ for query in queries:
     toAppend = list()
     for trip in Sorted:
         jpid = trainSet["journeyPatternId"][trip[1]]
-        route = trainSet["Trajectory"][trip[1]]
-        distance = len(trip[0])
-        toAppend.append( (jpid, distance, route) )
+        route = trains[trip[1]]
+        toAppend.append( (jpid, trip[0], route) )
     TestTrips.append(toAppend)
 
+    end = time.time()
+    duration = end - start
+    file.write("Time : " + str(duration) + "\n")
+
 i = 1
-for k in TestTrips:
-    print "Trip " + str(i)
+for query, test, file in izip(queries, TestTrips, fds):
+
+    # Draw query map
+    querylons = list()
+    querylats = list()
+    for lons, lats in query:
+        querylons.append(lons)
+        querylats.append(lats)
+
+    gmap = gmplot.GoogleMapPlotter(np.mean(querylats), np.mean(querylons), 13)
+
+    name = "./a2Plots/Test Trip " + str(i) + "/Query.html"
+    gmap.plot(querylats, querylons, 'green', edge_width=5)
+    gmap.draw(name)
+
     j = 1
-    for m in k:
-        print "\t" + "Neighbour " + str(j)
-        print "\t\tJP_ID: " + str(m[0])
-        print "\t\tMatching Points: " + str(m[1])
+    # for each neighbour draw neighbour map with green and common subsequence with red
+    for neighbour in test:
+
+        routelons = list()
+        routelats = list()
+        sbsqlons = list()
+        sbsqlats = list()
+        for lons, lats in neighbour[2]:
+            routelons.append(lons)
+            routelats.append(lats)
+        for lons, lats in neighbour[1]:
+            sbsqlons.append(lons)
+            sbsqlats.append(lats)
+        
+        gmapn = gmplot.GoogleMapPlotter(np.mean(routelats), np.mean(routelons), 13)
+
+        name = "./a2Plots/Test Trip " + str(i) + "/Neighbour " + str(j) + ".html"
+        file.write("Neighbour " + str(j) + " :\tMatchingPoints : " + str(len(neighbour[1])) + "\t" + " JPID : " + neighbour[0] + "\n")
+
+        gmapn.plot(routelats, routelons, 'green', edge_width=5)
+        gmapn.plot(sbsqlats, sbsqlons, 'red', edge_width=5)
+        gmapn.draw(name)
         j += 1
     i += 1
-    print "\n"
+    file.close()
+
+
 
 end = time.time
--- lcss.py
+++ lcss.py
@@ -4,22 +4,21 @@ from harversine import harvesine
 def lcss(u, v, match_error_margin):
 
     def bt(t, u, v, match_error_margin, i , j): # Backtrack through matrix t in order to find the lcss
-        if i == -1 or j == -1:
+        if i == 0 or j == 0:
             return []
-        if harvesine(u[i], v[j]) <= match_error_margin:
-            return bt(t, u, v, match_error_margin, i - 1, j - 1) + [u[i]]
+        if harvesine(u[i - 1], v[j - 1]) <= match_error_margin:
+            return bt(t, u, v, match_error_margin, i - 1, j - 1) + [v[j - 1]]
         elif t[i][j - 1] > t[i - 1][j]:
             return bt(t, u, v, match_error_margin, i, j - 1)
         else:
             return bt(t, u, v, match_error_margin, i - 1, j)
 
-    t = np.zeros( (len(u),len(v)) ) # LCSS computation table
+    t = np.zeros( (len(u) + 1,len(v) + 1) ) # LCSS computation table
 
-    for i in xrange(len(u)):
-        for j in xrange(len(v)):
-            if harvesine(u[i], v[j]) <= match_error_margin:
+    for i in xrange(1,len(u) + 1):
+        for j in xrange(1,len(v) + 1):
+            if harvesine(u[i - 1], v[j - 1]) <= match_error_margin:
                 t[i][j] = t[i - 1][j - 1] + 1
             else:
                 t[i][j] = max( [ t[i][j - 1], t[i - 1][j] ] )
-    
-    return bt(t, u, v, match_error_margin, len(u) - 1, len(v) - 1)
+    return bt(t, u, v, match_error_margin, len(u), len(v))
